6a602290b64694bef645d937e8bc18a2c157c252
Berk Erol <berk-erol@outlook.com>
2023-11-26 17:13:30 +0100

    Add release years and genres

--- gts.js
+++ gts.js
@@ -1,4 +1,4 @@
-/* global songs createTextRow createNumberRow createCheckboxCol createButtonGroupRow createDropdownCol createDatalist deleteOptionFromDatalist createRow createAlert keyDownHandler keyUpHandler */
+/* global songs createTextRow createNumberRow createCheckboxCol createButtonGroupRow createDropdownCol createDatalist deleteOptionFromDatalist createRow createModalButton createModal createAlert keyDownHandler keyUpHandler */
 const defaultStartTime = 0;
 const defaultDuration = 30;
 const defaultDel = true;
@@ -14,6 +14,9 @@ let timeout;
 
 window.locked = true;
 
+let currentSongs = [];
+let artistsAll = [];
+
 const form = document.getElementById('form');
 const rowClass = 'row justify-content-center';
 const colClass = 'col-5 col-md-4 col-lg-3 mb-3';
@@ -21,19 +24,28 @@ const buttonClass = 'col-6 col-md-4 col-lg-3 mb-3 my-auto';
 const firstRow = [['Start Time', 'startTime', '0', '600'], ['Duration', 'duration', '0', '30']];
 const secondRow = [[['primary dropdown-toggle', '', 'm', 'pen', '<span id="mode-text">Change <u>m</u>ode</span>'], 'mode', [['0', 'Artist & Title'], ['1', 'Only Artist'], ['2', 'Only Title']]], ['<u>D</u>elete song after round', 'del', 'd']];
 const thirdRow = [['Guess Artist', 'guessArtist'], ['Guess Title', 'guessTitle']];
-const fourthRow = [['success', 'if(!locked)window.guess()', 'g', 'search', '<u>G</u>uess'], ['danger', 'if(!locked)giveUp()', 'u', 'times', 'Give <u>U</u>p'], ['info', 'restart()', 'e', 'sync', 'R<u>e</u>start']];
+const fourthRow = [['success', 'if(!locked)window.guess()', 'g', 'search', '<u>G</u>uess'], ['danger', 'if(!locked)giveUp()', 'u', 'times', 'Give <u>U</u>p'], ['info', 'restart()', 'e', 'sync', 'R<u>e</u>start'], ['info', '', 's', 'cog', '<u>S</u>ettings']];
 form.appendChild(createNumberRow(rowClass, colClass, firstRow));
 form.appendChild(createRow(rowClass, [createDropdownCol(colClass, ...secondRow[0]), createCheckboxCol(colClass + ' my-auto', ...secondRow[1])]));
 form.appendChild(createTextRow(rowClass, colClass, thirdRow));
 form.appendChild(createRow(rowClass, [createButtonGroupRow(buttonClass, 'btn-group', fourthRow)]));
-const artistsAll = [];
-const titles = [];
+createModalButton(form.children[3].children[0].children[0], 3);
+let minYear = +songs[0][3];
+let maxYear = +songs[0][3];
+const genres = [];
 for (const song of songs) {
-  artistsAll.push(song[0]);
-  titles.push(song[1]);
+  genres.push(...song[4]);
+  if (+song[3] < minYear) {
+    minYear = +song[3];
+  }
+  if (+song[3] > maxYear) {
+    maxYear = +song[3];
+  }
 }
-form.appendChild(createDatalist('guessArtist', 'guessArtistDatalist', Array.from(new Set(artistsAll))));
-form.appendChild(createDatalist('guessTitle', 'guessTitleDatalist', titles));
+const uniqueGenres = Array.from(new Set(genres));
+uniqueGenres.unshift('All');
+const modalElements = [[['Min year', 'minYear', 1, 99999, 'number'], ['Max year', 'maxYear', 1, 99999, 'number']], [[['primary dropdown-toggle', undefined, 'e', 'music', 'G<u>e</u>nres'], 'genres-dropdown', uniqueGenres, 'dropdown-check']]];
+createModal(modalElements);
 document.querySelectorAll('.dropdown-item').forEach(e => {
   e.addEventListener('click', function () {
     document.getElementById('mode-text').innerText = this.innerText;
@@ -50,7 +62,24 @@ document.querySelectorAll('.dropdown-item').forEach(e => {
     }
   });
 });
+const checkboxGenreAll = document.querySelectorAll('#All.form-check-input')[0];
+const checkboxGenreOthers = document.querySelectorAll(':not(#All).form-check-input');
+checkboxGenreAll.addEventListener('click', function () {
+  if (this.checked) {
+    checkboxGenreOthers.forEach(e => {
+      e.checked = false;
+    });
+  }
+});
+checkboxGenreOthers.forEach(e => {
+  e.addEventListener('click', function () {
+    if (this.checked) {
+      checkboxGenreAll.checked = false;
+    }
+  });
+});
 resetInputs();
+save();
 restart();
 document.addEventListener('keydown', keyDownHandler);
 document.addEventListener('keyup', keyUpHandler);
@@ -62,6 +91,38 @@ function resetInputs () {
   document.getElementById('startTime').value = startTime;
   document.getElementById('duration').value = duration;
   document.getElementById('del').checked = del;
+  document.getElementById('minYear').value = minYear;
+  document.getElementById('maxYear').value = maxYear;
+  checkboxGenreAll.checked = true;
+}
+
+function save () {
+  const currentMinYear = +document.getElementById('minYear').value;
+  const currentMaxYear = +document.getElementById('maxYear').value;
+  const currentGenres = [];
+  if (checkboxGenreAll.checked) {
+    currentGenres.push(...uniqueGenres);
+  } else {
+    checkboxGenreOthers.forEach(e => {
+      if (e.checked) {
+        currentGenres.push(e.id);
+      }
+    });
+  }
+  currentSongs = [];
+  for (const song of songs) {
+    if (song[3] >= currentMinYear && song[3] <= currentMaxYear && song[4].some(genre => currentGenres.includes(genre))) {
+      currentSongs.push(song);
+    }
+  }
+  artistsAll = [];
+  const titles = [];
+  for (const song of currentSongs) {
+    artistsAll.push(song[0]);
+    titles.push(song[1]);
+  }
+  form.appendChild(createDatalist('guessArtist', 'guessArtistDatalist', Array.from(new Set(artistsAll))));
+  form.appendChild(createDatalist('guessTitle', 'guessTitleDatalist', titles));
 }
 
 window.guess = function () {
@@ -100,8 +161,8 @@ function restart () {
   startTime = +document.getElementById('startTime').value;
   duration = +document.getElementById('duration').value;
   del = document.getElementById('del').checked;
-  songIndex = Math.floor(Math.random() * songs.length);
-  song = songs[songIndex];
+  songIndex = Math.floor(Math.random() * currentSongs.length);
+  song = currentSongs[songIndex];
   window.clearTimeout(timeout);
   if (iframe) {
     iframe.remove();
@@ -129,7 +190,7 @@ function check (id, guess) {
 function end (className, text) {
   createAlert(className, text);
   if (del) {
-    songs.splice(songIndex, 1);
+    currentSongs.splice(songIndex, 1);
     artistsAll.splice(artistsAll.indexOf(song[0]), 1);
     if (artistsAll.indexOf(song[0]) === -1) {
       deleteOptionFromDatalist('guessArtistDatalist', song[0]);
